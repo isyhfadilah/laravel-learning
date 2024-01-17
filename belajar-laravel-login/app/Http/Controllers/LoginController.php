84a3bc18396063feb864814023fcdf201a548ae6
Aisyah Nur Fadilah <aisyahnfadilah73@gmail.com>
2024-01-17 14:15:06 +0700

    feat: add documentation post controller laravel crud

---
+++ belajar-laravel-login/app/Http/Controllers/LoginController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+// can handle authentication
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+
+// send message session error
+use Session;
+
+class LoginController extends Controller
+{
+    public function login()
+    {
+        // apakah user sdh login ?
+        if(Auth::check()) {
+            return redirect('home');
+        } else {
+            return view('login');
+        }
+    }
+    
+    public function actionlogin(Request $request) 
+    {
+        $data = [
+            'email' => $request->input('email'),
+            'password' => $request->input('password'),
+        ];
+
+        // // cek validasi login langsung ke table users
+        if(Auth::Attempt($data)) {
+            return redirect('home');
+        } else {
+            Session::flash('error', 'Email atau Password Salah!');
+            return redirect('/');
+        }
+
+        // Debugging statement: Log the input data
+        // Log::info('Input data:', $data);
+
+        // if(Auth::attempt($data)) {
+        //     // Debugging statement: Log a success message
+        //     Log::info('Login successful');
+
+        //     return redirect('home');
+        // } else {
+        //     // Debugging statement: Log a failure message
+        //     Log::error('Login failed', ['email' => $data['email']]);
+
+        //     Session::flash('error', 'Email atau Password Salah!');
+        //     return redirect('/');
+        // }
+    }
+
+    public function actionlogout()
+    {
+        Auth::logout();
+        return redirect('/');
+    }
+}
---
+++ belajar-laravel-login/routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\HomeController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', [LoginController::class, 'login'])->name('login');
+Route::post('actionlogin', [LoginController::class, 'actionlogin'])->name('actionlogin');
+
+Route::get('home', [HomeController::class, 'index'])->name('home')->middleware('auth');
+Route::get('actionlogout', [LoginController::class, 'actionlogout'])->name('actionlogout')->middleware('auth');
