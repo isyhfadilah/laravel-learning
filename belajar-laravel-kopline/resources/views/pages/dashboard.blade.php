8db277d84c18c5a50ec6a29fd7f93dbac4a3388f
Aisyah Nur Fadilah <aisyahnfadilah73@gmail.com>
2024-02-13 11:06:58 +0700

    feat: add button in dashboard

--- belajar-laravel-kopline/resources/views/pages/dashboard.blade.php
+++ belajar-laravel-kopline/resources/views/pages/dashboard.blade.php
@@ -6,11 +6,12 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Kopline | Dashboard</title>
     @vite('resources/css/app.css')
-    <link rel="stylesheet" href="{{ URL::asset('css/main.css'); }} ">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <link rel="stylesheet" href="{{ URL::asset('css/main.css'); }} ">
 </head>
 <body class="bg-stone-950">
     @include('components/sidebar')
+    @include('ui/dashboard/main')
     <script type="text/javascript" src="{{ URL::asset('js/main.min.js'); }} "></script>
 </body>
 </html>
---
+++ belajar-laravel-kopline/resources/views/ui/dashboard/main.blade.php
@@ -0,0 +1,3 @@
+<div class="mx-10 grid grid-cols-3 gap-4 text-white font-bold text-2xl capitalize">
+    @include('ui/dashboard/carddashboard')
+</div>
